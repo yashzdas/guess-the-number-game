7fd9afa0b873e8b677fdf66368b404ba9f13a8c7
Yash Das <28565044+yashzdas@users.noreply.github.com>
2017-07-31 17:14:23 +0530

    Add files via upload

---
+++ guess the no. game.py
@@ -0,0 +1,26 @@
+from random import randint
+
+
+def play_again():
+    var= input('Type: "sure" or "exit" ')
+    if var == "sure":
+        play()
+    elif var == "exit":
+        print('see you again..!')
+    else:
+        print('keyword not matching')
+        play_again()
+def play():
+    user= int(input('Guess a digit between 0 to 9: '))
+    comp= randint(0,10)
+    if user == comp:
+        print('win ',comp)
+    elif user<comp:
+        z= str(comp-user)
+        print(comp,' your number is '+z+' digit lower')
+    elif comp<user:
+        x= str(user-comp)
+        print(comp,' your number is '+x+' digit higher')
+        print('shall we play again..!')
+    play_again()
+play()
